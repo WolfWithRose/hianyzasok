47db3f1174cc29b8ae2e6647d82f9d9266c3486a
WolfWithRose <grimm.online7@gmail.com>
2022-03-01 14:18:32 +0100

    Mindjart kesz

--- hianyzasok/hianyzasok/Program.cs
+++ hianyzasok/hianyzasok/Program.cs
@@ -9,10 +9,18 @@ namespace hianyzasok
 {
     class Program
     {
+        static string hetnapja(int honap, int nap)
+        {
+            string[] napnev = { "vasarnap", "hetfo" , "kedd" , "szerda" , "csutortok" , "pentek" , "szombat"};
+            int[] napszam = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 335 };
+            int napsorszam = (napszam[honap - 1] + nap) % 7;
+            string hetnapja = napnev[napsorszam];
+            return hetnapja;
+        }
         class Naplo
         {
             public DateTime datum;
-            public List<(string, string)> hianyzas = new List<(string, string)>();
+            public List<(string, string)> hianyzasok = new List<(string, string)>();
         }
         static void Main(string[] args)
         {
@@ -27,10 +35,42 @@ namespace hianyzasok
                 do
                 {
                     adat = sorok[++i].Split(' ');
-                    n.hianyzas.Add((adat[0] + " " + adat[1], adat[2]));
-                } while (i < sorok.Count() && sorok[++i].Split(' ')[0] != "#");
+                    n.hianyzasok.Add((adat[0] + " " + adat[1], adat[2]));
+                } while (i < sorok.Count() - 1 && sorok[i + 1].Split(' ')[0] != "#");
                 lista.Add(n);
             }
+
+            Console.WriteLine("2. feladat");
+            int db = 0;
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                db += lista[i].hianyzasok.Count();
+            }
+            Console.WriteLine($"A naplóban {db} bejegyzés van.");
+            Console.WriteLine("3. feladat");
+            int igazolt = 0;
+            int igazolatlan = 0;
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                for (int j = 0; j < lista[i].hianyzasok.Count(); j++)
+                {
+                    igazolatlan += lista[i].hianyzasok[j].Item2.Count(x => x == 'I');
+                    igazolt += lista[i].hianyzasok[j].Item2.Count(x => x == 'X');
+                }
+            }
+            Console.WriteLine($"Az igazolt hiányzások száma {igazolt}, az igazolatlanoké {igazolatlan} óra.");
+            Console.WriteLine("5. feladat");
+            Console.Write("A hónap sorszáma=");
+            int honap = int.Parse(Console.ReadLine());
+            Console.Write("A nap sorszáma=");
+            int nap = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Azon a napon {hetnapja(honap, nap)} volt.");
+            Console.WriteLine("6. feladat");
+            Console.Write("A nap neve=");
+            string napneve = Console.ReadLine();
+            Console.Write("A óra sorszáma=");
+            int óra = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Ekkor összesen {lista.First(x => hetnapja(x.datum.Month, x.datum.Day) == napneve).hianyzasok.Sum(x => x.Item2[óra] == 'I' || x.Item2[óra] == 'X')} óra hiányzás történt.");
         }
     }
 }
